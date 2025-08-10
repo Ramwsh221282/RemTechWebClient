5584179930f55aa0ece2125eb32d01095c0916f6
Ramwsh <jimkrauz@gmail.com>
2025-08-11 02:45:16 +0700

    added confirmation dialog if parser is enabled

--- src/src/app/pages/scrapers-management-page/components/scrapers-management-concrete-scraper-page/components/scraper-deactivate-button/scraper-deactivate-button.component.ts
+++ src/src/app/pages/scrapers-management-page/components/scrapers-management-concrete-scraper-page/components/scraper-deactivate-button/scraper-deactivate-button.component.ts
@@ -11,18 +11,19 @@ import {
 import { Scraper } from '../../../scrapers-management-settings-page/types/Scraper';
 import { VehicleScrapersService } from '../../../scrapers-management-settings-page/services/vehicle-scrapers.service';
 import { Toast } from 'primeng/toast';
-import { MessageService } from 'primeng/api';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ParserStateChangeResult } from '../../../scrapers-management-settings-page/types/ParserStateChangedResult';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MessageServiceUtils } from '../../../../../../shared/utils/message-service-utils';
+import { ConfirmDialog } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-scraper-deactivate-button',
-  imports: [Toast],
+  imports: [Toast, ConfirmDialog],
   templateUrl: './scraper-deactivate-button.component.html',
   styleUrl: './scraper-deactivate-button.component.scss',
-  providers: [MessageService],
+  providers: [MessageService, ConfirmationService],
 })
 export class ScraperDeactivateButtonComponent {
   @Output() scraperDeactivated: EventEmitter<Scraper> = new EventEmitter();
@@ -33,14 +34,47 @@ export class ScraperDeactivateButtonComponent {
   private readonly _messageService: MessageService;
   private readonly _service: VehicleScrapersService;
   private readonly _destroyRef: DestroyRef = inject(DestroyRef);
-  constructor(messageService: MessageService, service: VehicleScrapersService) {
+  private readonly _confirmationService: ConfirmationService;
+  constructor(
+    messageService: MessageService,
+    service: VehicleScrapersService,
+    confirmationService: ConfirmationService,
+  ) {
+    this._confirmationService = confirmationService;
     this._messageService = messageService;
     this._service = service;
     this._scraper = signal(VehicleScrapersService.defaultScraper());
   }
 
-  public click(): void {
+  public click($event: Event): void {
     const current: Scraper = this._scraper();
+    if (current.state === 'Работает') {
+      this._confirmationService.confirm({
+        target: $event.target as EventTarget,
+        message: 'Вы действительно хотите остановить работу парсера?',
+        header: 'Подтверждение',
+        closable: true,
+        closeOnEscape: true,
+        icon: 'pi pi-exclamation-triangle',
+        rejectButtonProps: {
+          label: 'Нет',
+          severity: 'secondary',
+          outlined: true,
+        },
+        acceptButtonProps: {
+          label: 'Да',
+        },
+        accept: () => {
+          this.invokeChangeState(current);
+        },
+        reject: () => {},
+      });
+    } else {
+      this.invokeChangeState(current);
+    }
+  }
+
+  private invokeChangeState(current: Scraper): void {
     this._service
       .changeState(current, { stateSwitch: false })
       .pipe(takeUntilDestroyed(this._destroyRef))
